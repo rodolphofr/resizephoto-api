c0df0b4a8d5901f8d9bc7989556a44c415a71085
Rodolpho Rodrigues <rodolpho.frodrigues@gmail.com>
2018-01-12 12:30:21 -0200

    * send tests

--- spec/api/v1/image_controller_spec.rb
+++ spec/api/v1/image_controller_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
+require File.join(Rails.root, "app/services/images/image_writer_service")
 
 RSpec.describe Api::V1::ImageController, type: :controller do
+  before(:all) { ImageWriterService.new.populate }
 
-  describe "#index" do
+  describe '#index' do
     it 'list 10 url images' do
       get :index
       json = JSON.parse(response.body)
-
       expect(response).to be_success
       expect(json["images"].length).to eq(10)
     end
   end
 
-  describe "#show" do
+  describe '#show' do
+    let(:first_image) { Image.first }
+
     it 'get image by id' do
-      create(:basic_image)
-      get :show, params: { id: basic_image._id }
+      response = get(:show, params: { id: first_image._id.to_s })
+      expect(response).not_to be_nil
 
-      response = JSON.parse(response.body)
-
-      expect(response).to be_success
-      expect(response['image'].length).to eq(1)
+      json = JSON.parse(response.body)
+      expect(json).to have_key('image')
+      expect(json['image']).not_to be_empty
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,5 @@
-Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each { |file| require file }
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -36,6 +33,16 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.before(:suite) do
+    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.start
+  end
+
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
