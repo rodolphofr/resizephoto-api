56c40e68ab29f811feec8c6a70bf9475d7b8a706
Rodolpho Rodrigues <rodolpho.frodrigues@gmail.com>
2018-01-12 01:01:04 -0200

    * Modeled Image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,12 @@
 class Image
   include Mongoid::Document
+
+  store_in collection: 'images'
+
   field :url, type: String
   field :formats, type: Array
+
+  index({ url: 1 }, { unique: true, name: 'url_index' })
+
+  validates_uniqueness_of :url
 end
